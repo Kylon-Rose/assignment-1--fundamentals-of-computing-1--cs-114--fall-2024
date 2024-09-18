3faeb9e4bf8949d281a26c458b97a34ef266382d
Kylon-Rose <kylonrose12345@gmail.com>
2024-09-18 19:58:48 -0400

    Added Temp Converter

--- Assignment1.java
+++ Assignment1.java
@@ -9,5 +9,14 @@ public class Assignment1 {
     System.out.println("KK  KK        RR    RR");
     System.out.println("KK    KK      RR      RR");
     System.out.println("KK      KK    RR        RR");
+
+   Scanner scanner = new Scanner(System.in);
+   
+    System.out.println("Please enter a number in Fahrenheit");
+    double fahrenheit = scanner.nextDouble();
+
+    double celsius = (fahrenheit - 32) * 5 / 9;
+
+    System.out.println("The temp is: " + celsius);
   }
 }
