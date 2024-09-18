c55b6de81f9ac9d0a3f231d242dce4e8e6be91da
Kylon-Rose <kylonrose12345@gmail.com>
2024-09-18 19:30:16 -0400

    printed initials

--- Assignment1.java
+++ Assignment1.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
+    System.out.println("KK      KK    RRRRRRRRRR"); 
+    System.out.println("KK    KK      RR        RR");
+    System.out.println("KK  KK        RR          RR");
+    System.out.println("KK KK         RR        RR");
+    System.out.println("KK  KK        RR    RR");
+    System.out.println("KK    KK      RR      RR");
+    System.out.println("KK      KK    RR        RR");
   }
 }
