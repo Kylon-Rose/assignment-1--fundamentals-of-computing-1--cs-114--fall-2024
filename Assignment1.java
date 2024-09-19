57ec81ffb9d2994e28f0c0b89ca43103b8d2082d
Kylon-Rose <kylonrose12345@gmail.com>
2024-09-18 20:57:47 -0400

    added reverse 5 to 3 letters

--- Assignment1.java
+++ Assignment1.java
@@ -18,5 +18,14 @@ public class Assignment1 {
     double celsius = (fahrenheit - 32) * 5 / 9;
 
     System.out.println("The temp is: " + celsius);
+
+    System.out.println("Please enter a 5-character string");
+
+    String original = "Kylon";
+    String reversed = new StringBuilder(original).reverse(). toString();
+    String trimmed = reversed.substring (1, reversed.length() - 1);
+    System.out.println(trimmed);
+
+  scanner.close(); 
   }
 }
